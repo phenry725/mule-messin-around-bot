4d4eeef930827ec31b83f72febc012c2f7c45d22
Matt Burdan <burdz@burdz.net>
2020-05-10 04:43:19 -0700

    [cmd] add Handler

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,39 @@
 //
 // use rand::Rng;
 use serenity::client::Client;
-use serenity::model::channel::Message;
-use serenity::prelude::{EventHandler, Context};
-use serenity::framework::standard::{
-    StandardFramework,
-    CommandResult,
-    macros::{
-        command,
-        group
-    }
+use serenity::prelude::{Context, EventHandler};
+use serenity::{
+    framework::{
+        standard::macros::{command, group},
+        standard::CommandResult,
+        StandardFramework,
+    },
+    model::{channel::Message, gateway::Ready},
 };
 
 #[group]
-#[commands(ping,stupidspeak)]
+#[commands(ping, stupidspeak)]
 struct General;
 
 use std::env;
 
 struct Handler;
 
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
 
 fn main() {
     // Login with a bot token from the environment
     let mut client = Client::new(&env::var("DISCORD_TOKEN").expect("token"), Handler)
         .expect("Error creating client");
-    client.with_framework(StandardFramework::new()
-        .configure(|c| c.prefix("!")) // set the bot's prefix to "~"
-        .group(&GENERAL_GROUP));
+    client.with_framework(
+        StandardFramework::new()
+            .configure(|c| c.prefix("!")) // set the bot's prefix to "~"
+            .group(&GENERAL_GROUP),
+    );
 
     // start listening for events by starting a single shard
     if let Err(why) = client.start() {
@@ -44,14 +49,12 @@ fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
     Ok(())
 }
 
-
 #[command]
 fn stupidspeak(ctx: &mut Context, msg: &Message) -> CommandResult {
     msg.reply(ctx, &msg.content)?;
 
     Ok(())
 }
-
 
 // fn randomNumber(start: int, end: int) -> int {
 //     return rng.gen_range(0, 10));
